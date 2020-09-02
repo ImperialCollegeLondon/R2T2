8a41985ab945300a618f7541ec7dd8738fc15451
Raniere Silva <raniere@rgaiacs.com>
2020-09-02 12:28:06 -0300

    Provide more examples in documentation

---
+++ docs/examples/minimal-method.py
@@ -0,0 +1,10 @@
+from r2t2 import add_reference
+
+class MyGreatClass():
+
+     @add_reference(short_purpose="Original implementation of R2T2",
+               reference="Diego Alonso-Álvarez, et al."
+               "(2018, February 27). Solcore (Version 5.1.0). Zenodo."
+               "http://doi.org/10.5281/zenodo.1185316")
+     def my_great_function(self):
+          pass
