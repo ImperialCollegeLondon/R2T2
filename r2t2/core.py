500f583de71d49e6d36aecb4426f62d222c256bc
Chas Nelson <cvs@chasnelson.co.uk>
2020-09-02 14:54:43 +0100

    add ability to add_reference using bibtex_key

--- r2t2/core.py
+++ r2t2/core.py
@@ -47,7 +47,11 @@ BIBLIOGRAPHY: Biblio = Biblio()
 
 
 def add_reference(
-    *, short_purpose: str, reference: Optional[str] = None, doi: Optional[str] = None
+    *,
+    short_purpose: str,
+    reference: Optional[str] = None,
+    doi: Optional[str] = None,
+    bibtex_key: Optional[str] = None,
 ) -> Callable:
     """Decorator to link a reference to a function or method.
 
@@ -62,16 +66,20 @@ def add_reference(
         short_purpose (str): Identify the thing being referenced.
         reference (Optional, str): The reference itself, as a plain text string.
         doi (Optional, str): DOI of the reference.
+        bibtex_key (Optional, str): key of the reference in bibtex file.
 
     Returns:
         The decorated function.
     """
-    if reference and doi:
+    if (reference and doi) or (reference and bibtex_key) or (doi and bibtex_key):
+        # TODO make this conditional more flexible
         raise ValueError("Only one method for providing the reference is allowed.")
     elif reference:
         ref = f"[plain]{reference}"
     elif doi:
         ref = f"[doi]{doi}" if "doi.org" in doi else f"[doi]https://doi.org/{doi}"
+    elif bibtex_key:
+        ref = f"[bibtex]{bibtex_key}"
     else:
         raise ValueError("No reference information provided!")
 
