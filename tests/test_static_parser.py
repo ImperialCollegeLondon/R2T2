6e2cf6b61fffa16a58e1f240789b551ef905cc84
Jez Cope <j.cope@erambler.co.uk>
2020-09-02 15:32:10 +0100

    Add test for locate_references globbing

--- tests/test_static_parser.py
+++ tests/test_static_parser.py
@@ -4,7 +4,8 @@ from r2t2.static_parser import locate_references
 
 
 HERE = Path(__file__).parent
-SAMPLE_PATH = HERE / "fixtures/sample_code.py"
+FIXTURES = HERE / "fixtures"
+SAMPLE_PATH = FIXTURES / "sample_code.py"
 
 
 class TestLocateReferences:
@@ -14,4 +15,8 @@ class TestLocateReferences:
 
     def test_accepts_path(self, bib_with_tracking):
         locate_references(SAMPLE_PATH)
+        assert "Great British Roasts, 2019" in bib_with_tracking.references
+
+    def test_globs_for_folder(self, bib_with_tracking):
+        locate_references(FIXTURES)
         assert "Great British Roasts, 2019" in bib_with_tracking.references
