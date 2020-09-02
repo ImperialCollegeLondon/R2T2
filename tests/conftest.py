7b7cec78696dbe742560b4e38b5c2e791d20a0ac
Daniel Ecer <de-code@users.noreply.github.com>
2020-09-02 12:02:19 +0100

    linting

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import pytest
 from py._path.local import LocalPath
 
+
 @pytest.fixture(scope='session', autouse=True)
 def setup_logging():
     for name in {'r2t2', 'tests'}:
