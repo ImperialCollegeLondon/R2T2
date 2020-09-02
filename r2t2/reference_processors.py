bfd2abd462658ff06290ee9bb6f2046dc94a61d2
Diego <d.alonso-alvarez@imperial.ac.uk>
2020-09-02 12:49:18 +0100

    Skeleton of the process reference system

---
+++ r2t2/reference_processors.py
@@ -0,0 +1,66 @@
+from typing import Callable, Dict
+from functools import partial, lru_cache
+from abc import ABC, abstractmethod
+import re
+
+
+PROCESSORS: Dict[str, Callable]
+"""Dictionary of available processors."""
+
+
+def register_processor(f: Callable = None, name: str = None) -> Callable:
+    """Register a reference processor.
+
+    Args:
+        f (callable): Function processing the reference string.
+        name (str): Name of the processor.
+
+    Returns:
+        The input processor.
+    """
+    if f is None:
+        return partial(register_processor, name=name)
+
+    name = name if name else f.__name__
+    PROCESSORS[name] = f
+
+    return f
+
+
+@lru_cache
+def process_reference(ref: str, output_format: str = "plain"):
+    """Process the reference string in the chosen format.
+
+    This function is cached, so multiple calls with identical arguments are processed
+    faster, specially useful when information needs to be read from a file or retrieved
+    from the internet.
+
+    Args:
+        ref (str): The reference string. Should start with [XXX], where XXX is one of
+            the known processors.
+        output_format (str): The format the reference should be provided
+            ("plain" or "bibtex").
+
+    Returns:
+        The reference in the requested format.
+    """
+    match = re.findall(r'([\w.-/\\:\s]+)', ref)
+    if len(match) != 2:
+        raise ValueError(f"Could not process reference {ref}")
+
+    return PROCESSORS[match[0]](match[1])
+
+
+class ProcessorBase(ABC):
+    """Base class for all reference processors."""
+
+    def __init__(self, ref):
+        self._ref = ref
+
+    @abstractmethod
+    def plain_text(self):
+        """Return the reference as plain text in a sensible format."""
+
+    @abstractmethod
+    def bibtex(self):
+        """Return the reference as a bibtex entry."""
