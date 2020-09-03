85259fa049e865f69b45d94b06aa94f8a81ed27f
Daniel Ecer <de-code@users.noreply.github.com>
2020-09-03 11:40:30 +0100

    added main tests

--- r2t2/__main__.py
+++ r2t2/__main__.py
@@ -1,7 +1,9 @@
 import argparse
+import logging
 import os
 import sys
 from pathlib import Path
+from typing import List
 
 from .static_parser import locate_references
 from .runtime_tracker import runtime_tracker
@@ -12,83 +14,97 @@ from .docstring_reference_parser import (
 )
 
 
-parser = argparse.ArgumentParser()
+def parse_args(argv: List[str] = None) -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
 
-parser.add_argument(
-    "-f",
-    "--format",
-    default="terminal",
-    type=str,
-    help=f"Format of the output. Default: Terminal. "
-    f"Valid options are: {','.join(list(REGISTERED_WRITERS.keys()))}.",
-)
-
-parser.add_argument(
-    "-o",
-    "--output",
-    default=None,
-    type=str,
-    help="File to save the references into. Ignored if format is 'Terminal'."
-    "Default: [target folder]/references.",
-)
-
-parser.add_argument(
-    "-s",
-    "--static",
-    action="store_true",
-    help="When processing a file, indicates if a static analysis should be done rather "
-    "than a runtime analysis, the default behaviour for files.",
-)
-
-parser.add_argument(
-    "--docstring",
-    action="store_true",
-    help="Also parse docstrings.",
-)
-
-parser.add_argument(
-    "target",
-    default=".",
-    nargs="?",
-    type=str,
-    help="Target file or folder to analyse. If the target is a python file, this is "
-    "run as a script and the references provided are those found at runtime. "
-    "Default: Current directory.",
-)
-
-parser.add_argument(
-    "args",
-    nargs=argparse.REMAINDER,
-    help="Input arguments for running the target script, if needed.",
-)
-
-args = parser.parse_args()
-
-if args.format not in REGISTERED_WRITERS:
-    print(
-        f"ERROR: Unrecognized output formats. "
-        f"Valid options for -f are: {', '.join(list(REGISTERED_WRITERS.keys()))}"
+    parser.add_argument(
+        "-f",
+        "--format",
+        default="terminal",
+        type=str,
+        help=f"Format of the output. Default: Terminal. "
+        f"Valid options are: {','.join(list(REGISTERED_WRITERS.keys()))}.",
     )
-    sys.exit()
 
-if args.output is None:
-    if os.path.isdir(args.target):
-        output = Path(args.target) / "references"
-    else:
-        output = Path(args.target).parent / "references"
-elif os.path.isdir(args.output):
-    msg = f"Output must be file, not a directory. Now it is {args.output}"
-    raise IsADirectoryError(msg)
-else:
-    output = Path(args.output)
+    parser.add_argument(
+        "-o",
+        "--output",
+        default=None,
+        type=str,
+        help="File to save the references into. Ignored if format is 'Terminal'."
+        "Default: [target folder]/references.",
+    )
 
-if os.path.isdir(args.target) or args.static:
-    locate_references(args.target)
-    if args.docstring:
-        parse_and_add_docstring_references_from_files(
-            expand_file_list(args.target)
+    parser.add_argument(
+        "-s",
+        "--static",
+        action="store_true",
+        help="When processing a file, indicates if a static analysis should be done rather "
+        "than a runtime analysis, the default behaviour for files.",
+    )
+
+    parser.add_argument(
+        "--docstring",
+        action="store_true",
+        help="Also parse docstrings.",
+    )
+
+    parser.add_argument(
+        "target",
+        default=".",
+        nargs="?",
+        type=str,
+        help="Target file or folder to analyse. If the target is a python file, this is "
+        "run as a script and the references provided are those found at runtime. "
+        "Default: Current directory.",
+    )
+
+    parser.add_argument(
+        "args",
+        nargs=argparse.REMAINDER,
+        help="Input arguments for running the target script, if needed.",
+    )
+
+    args = parser.parse_args(argv)
+
+    if args.format not in REGISTERED_WRITERS:
+        print(
+            f"ERROR: Unrecognized output formats. "
+            f"Valid options for -f are: {', '.join(list(REGISTERED_WRITERS.keys()))}"
         )
-else:
-    runtime_tracker(args.target, args.args)
+        sys.exit()
+    return args
 
-REGISTERED_WRITERS[args.format](output)
+
+def run(args: argparse.Namespace):
+    if args.output is None:
+        if os.path.isdir(args.target):
+            output = Path(args.target) / "references"
+        else:
+            output = Path(args.target).parent / "references"
+    elif os.path.isdir(args.output):
+        msg = f"Output must be file, not a directory. Now it is {args.output}"
+        raise IsADirectoryError(msg)
+    else:
+        output = Path(args.output)
+
+    if os.path.isdir(args.target) or args.static:
+        locate_references(args.target)
+        if args.docstring:
+            parse_and_add_docstring_references_from_files(
+                expand_file_list(args.target)
+            )
+    else:
+        runtime_tracker(args.target, args.args)
+
+    REGISTERED_WRITERS[args.format](output)
+
+
+def main(argv: List[str] = None):
+    args = parse_args(argv)
+    run(args)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level='INFO')
+    main()
---
+++ tests/test__main__.py
@@ -0,0 +1,60 @@
+import pytest
+
+from r2t2.__main__ import (
+    parse_args,
+    main
+)
+
+
+VALID_FORMAT = 'markdown'
+INVALID_FORMAT = 'other'
+
+
+class TestParseArgs:
+    def test_should_not_fail_on_valid_format(self):
+        args = parse_args([
+            '--format=%s' % VALID_FORMAT,
+            'docs/examples'
+        ])
+        assert args.format == VALID_FORMAT
+
+    def test_should_fail_on_invalid_format(self):
+        with pytest.raises(SystemExit):
+            parse_args([
+                '--format=%s' % INVALID_FORMAT,
+                'docs/examples'
+            ])
+
+
+@pytest.mark.usefixtures('bibliography')
+class TestMain:
+    def test_should_not_fail_on_static_analysis_of_examples_including_docstrings(
+        self
+    ):
+        main([
+            '--static',
+            '--docstring',
+            'docs/examples'
+        ])
+
+    def test_should_not_fail_on_static_analysis_of_examples_ignoring_docstrings(
+        self
+    ):
+        main([
+            '--static',
+            'docs/examples'
+        ])
+
+    def test_should_not_fail_on_runtime_analysis_of_examples(self):
+        main([
+            'docs/examples'
+        ])
+
+    def test_should_raise_error_with_invalid_writer(
+        self
+    ):
+        main([
+            '--static',
+            '--docstring',
+            'docs/examples'
+        ])
